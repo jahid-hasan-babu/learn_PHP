0b75e192055c73867b6056a1ddbd1e12bb84bb18
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-07-17 11:28:39 +0600

    learn comment

---
+++ class-05/index.php
@@ -0,0 +1,13 @@
+<?php
+//learn comment
+//single comment
+#single comment
+/*
+*multiline
+*comment 
+*/
+$firstPrice = 10;
+$secondPrice = 20;
+$totalPrice = $firstPrice + $secondPrice;
+echo $totalPrice
+?>
