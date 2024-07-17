6dc0c337be0657045b54b1d6c3fe041da6268aed
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-07-17 11:16:13 +0600

    variable-done

--- class-04/index.php
+++ class-04/index.php
@@ -19,8 +19,19 @@
     $price = 100 ;
     echo $price;
     echo $name;
+
+    // যদি variable print করতে চাই তাহলে "" দিতে হবে
+    echo " Hello $name your amount is $price ";
+    //আর  যদি variable single '' print করতে চাই তাহলে . দিতে হবে
+    echo ' Hello' .$name,' your amount is' .$price ;
+    echo " Hello " .$name." your amount is".$price."";
+
+    $first_name = "Rabbi";
+    $name = "first_name";
+    echo $$name;
     
     ?>
+    <h1>Hello <?php echo $first_name;?>, Your amount is <?php echo $price;?></h1>
 </body>
 
 </html>
