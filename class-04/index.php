2d4ec4c52a09980f01347173d4fb34efaac7ee48
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-07-17 10:49:45 +0600

    first-commit

---
+++ class-04/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class 04</title>
+</head>
+
+<body>
+    <h1><?php echo "hello ";?>Jahid</h1>
+    <?php
+    
+    
+    echo "hello ";
+    echo "Jahid ";
+    // variable 
+    $name = " Hasan";
+    $price = 100 ;
+    echo $price;
+    echo $name;
+    
+    ?>
+</body>
+
+</html>
