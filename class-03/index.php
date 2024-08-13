359df738b0b51ba709f6e3bdabaaa08aef0bace2
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-08-13 10:56:10 +0600

    class-04done

--- class-03/index.php
+++ class-03/index.php
@@ -1,11 +1,180 @@
 <?php
 
-$names = [
-    'Ranim' =>[122,478],
-    1=>'Karim'
-];
-echo "<pre>";
-print_r($names);
-echo "</pre>";
+// $names = [
+//     'Ranim' =>[122,478],
+//     1=>'Karim'
+// ];
+// echo "<pre>";
+// print_r($names);
+// echo "</pre>";
+
+// if(condition){
+//     statement;
+// }
+
+// if(condition){
+//     statement;
+// }else{
+//     statement;
+// }
+
+
+// if(condition){
+//     statement;
+// }else if(condition){
+//     statement;
+// }else{
+//     statement;
+// }
+
+// if(condition):
+
+// elseif(condition):
+
+// else:
+
+// endif
+
+
+
+// switch(statement){
+//     case(condition):
+//         statement;
+//         break;
+//     case(condition):
+//         statement;
+//         break;
+//     default:
+// }
+
+
+
+// $number1 = 130;
+// $number2 = 100;
+// $number3 = 120;
+
+// //big number
+// if($number1 > $number2 && $number1 > $number3){
+//     echo $number1 . "<br/>";
+// }else if($number2 > $number1 && $number2 > $number3){
+//     echo $number2 . "<br/>";
+// }else{
+//    echo $number3 . "<br/>";
+// }
+
+
+// // small number
+// if($number1 < $number2 && $number1 < $number3){
+//     echo $number1 . "<br/>";
+// }else if($number2 < $number1 && $number2 < $number3){
+//     echo $number2 . "<br/>";
+// }else{
+//    echo $number3 . "<br/>";
+// }
+// //middle number
+// if($number1 < $number2 && $number1 > $number3){
+//     echo $number1 . "<br/>";
+// }else if($number2 < $number1 && $number2 > $number3){
+//     echo $number2 . "<br/>";
+// }else{
+//    echo $number3 . "<br/>";
+// }
+
+
+// $name = "Jahid";
+// $j_roll = 2024;
+// $age = 20;
+// $class = 10;
+
+// $k_name = "Karim";
+// $k_roll = 2025;
+// $k_age = 18;
+// $k_class = 8;
+//nested if
+// if($j_roll == 2024 || $k_roll == 2025){
+//     if($class == 10){
+//         echo "Student of class 10: <br/> ";
+//         echo "Student Name $name. Age = $age <br/>";
+//     }
+//     if($k_class == 8){
+//         echo "Student of class 8: <br/>";
+//         echo "Student Name $k_name. Age = $k_age <br/>";
+//     }
+// }
+
+
+//leap year
+
+// $year = 2023;
+
+// if($year % 4 == 0){
+//     echo $year." is a leap year <br/>";
+// }else{
+//     echo $year." is not a leap year <br/>";
+// }
+//switch case
+
+
+
+// $number = 59;
+// switch($number){
+//     case($number >= 80):
+//         echo "Your grade is A+ <br/>";
+//         break;
+//     case($number >= 70):
+//         echo "Your grade is A <br/>";
+//         break;
+//     case($number >= 60):
+//         echo "Your grade is A- <br/>";
+//         break;
+//     default: 
+//     echo "Your are fail <br/>";
+// }
+
+//home work
+
+
+$number = 29;
+if($number % 2 == 0){
+    echo "$number is Even. <br/>";
+}else{
+    echo "$number is Odd. <br/>";
+}
+
+switch($number){
+    case($number % 2 ==0):
+        echo "$number is Even. <br/>";
+        break;
+    default:
+        echo "$number is Odd. <br/>";
+}
+
+$letter = "F";
+switch($letter){
+    case($letter == 'a' || $letter == 'A'):
+        echo "$letter is a vowel<br/>";
+        break;
+    case($letter == 'e' || $letter == 'E'):
+        echo "$letter is a vowel<br/>";
+        break;
+    case($letter == 'i' || $letter == 'I'):
+        echo "$letter is a vowel<br/>";
+        break;
+    case($letter == 'o' || $letter == 'O'):
+        echo "$letter is a vowel<br/>";
+        break;
+    case($letter == 'u' || $letter == 'U'):
+        echo "$letter is a vowel<br/>";
+        break;
+    default: 
+    echo "$letter is a consonant<br/>";
+}
+
+$name = "";
+//ternary operator
+$result = $name ? $name : "Name not found";
+//null operator
+$result = $name ?? "Name not found";
+echo $result;
 
 ?>
--- class-04/index.php
+++ class-04/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -12,26 +12,140 @@
     <?php
     
     
-    echo "hello ";
-    echo "Jahid ";
-    // variable 
-    $name = " Hasan";
-    $price = 100 ;
-    echo $price;
-    echo $name;
+    // echo "hello ";
+    // echo "Jahid ";
+    // // variable 
+    // $name = " Hasan";
+    // $price = 100 ;
+    // echo $price;
+    // echo $name;
 
     // যদি variable print করতে চাই তাহলে "" দিতে হবে
-    echo " Hello $name your amount is $price ";
+    // echo " Hello $name your amount is $price ";
     //আর  যদি variable single '' print করতে চাই তাহলে . দিতে হবে
-    echo ' Hello' .$name,' your amount is' .$price ;
-    echo " Hello " .$name." your amount is".$price."";
+    // echo ' Hello' .$name,' your amount is' .$price ;
+    // echo " Hello " .$name." your amount is".$price."";
 
-    $first_name = "Rabbi";
-    $name = "first_name";
-    echo $$name;
+    // $first_name = "Rabbi";
+    // $name = "first_name";
+    // echo $$name;
     
     ?>
     <h1>Hello <?php echo $first_name;?>, Your amount is <?php echo $price;?></h1>
 </body>
 
-</html>
+</html> -->
+
+
+<!-- loop -->
+<?php
+
+// for(start;end;increment/decrement){
+//     statement;
+//  }
+
+
+// for($i = 1; $i<= 10; $i++){
+//     echo $i ."<br/>";
+// }
+
+// for($i = 1; $i<= 10; $i++){
+//     echo $i ."<br/>";
+//     if($i == 7){
+//         break;
+//     }
+    
+// }
+// for($i = 1; $i<= 10; $i++){
+   
+//     if($i == 7){
+//         break;
+//     }
+//     echo $i ."<br/>";
+// }
+
+
+
+// for($i = 1; $i<= 10; $i++){
+//     if($i == 7 || $i == 8){
+//         continue;
+//     }
+//     echo $i ."<br/>";
+// }
+
+
+// for($y = 2000; $y <= 2024; $y++){
+//     if($y%4 == 0){
+//         echo $y. "Is a leap year <br/>";
+//     }else{
+//         echo $y. "Is not a leap year <br/>";
+//     }
+// }
+
+// for($i = 1; $i<= 10; $i++){
+//     for($x = 0; $x <= $i; $x++){
+//         echo "*";
+//     }
+//     echo "<br/>";
+// }
+
+
+// for($i = 10; $i>= 1; $i--){
+//     for($x = 0; $x <= $i; $x++){
+//         echo "*";
+//     }
+//     echo "<br/>";
+// }
+
+// $height = 5; // Number of levels in the pyramid
+
+// for ($i = 1; $i <= $height; $i++) {
+//     // Print leading spaces
+//     for ($j = $i; $j < $height; $j++) {
+//         echo "&nbsp;";
+//     }  
+//     // Print asterisks
+//     for ($k = 1; $k <= (2 * $i - 1); $k++) {
+//         echo "*";
+//     }
+//     echo "<br/>";
+// }
+
+// echo "Even number from 1 to 100 :" . PHP_EOL;
+// for ($i = 1; $i <= 100 ; $i ++){
+//     if($i % 2 == 0){
+//         echo  $i .", ";
+//     }
+// }
+
+// echo "<br/>";
+
+// echo "Odd number from 1 to 100 :" . PHP_EOL;
+// for ($i = 1; $i <= 100 ; $i ++){
+//     if($i % 2 !== 0){
+//         echo  $i .", ";
+//     }
+// }
+
+//while loop
+
+// $number = 10;
+// while($number >0){
+//     echo $number . "<br/>";
+//     $number --;
+// }
+
+// $number = 1;
+// while($number <=10){
+//     echo $number . "#";
+//     $number ++;
+// }
+
+
+$x=1;
+do{
+    echo $x. "<br/>";
+    $x++;
+}while($x<1);
+
+?>
